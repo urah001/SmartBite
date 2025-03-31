2d4ec57ec54a833299c4220465f6e4dbacc0f40d
STEPHEN SAMUEL URAH <STEPHENSAMUEL063@GMAIL.COM>
2025-03-31 13:57:15 +0100

    added login signup component

---
+++ app/authComponent/page.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
+
+const Auth = () => {
+  return (
+    <header className="flex-row sticky justify-end">
+      <Button asChild className="ml-4">
+        <Link href={"/login"}>login</Link>
+      </Button>
+      <Button asChild className="ml-4">
+        <Link href={"/signup"}>signup</Link>
+      </Button>
+    </header>
+  );
+};
+
+export default Auth;
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { CartProvider } from "@/components/ui/cart-provider";
 import LandingHeader from "@/components/ui/landingHeader";
-import Header from "@/components/ui/header";
+//import Header from "@/components/ui/header";
+import Auth from "./authComponent/page";
 //import Header from "@/components/ui/header";
 //import Footer from "@/components/ui/footer";
 
@@ -36,7 +37,7 @@ export default function RootLayout({
         <CartProvider>
           <div className="flex min-h-screen flex-col bg-[url('/student.jpg')] bg-cover bg-center bg-no-repeat">
             {/* <Header /> */}
-            {isUserLogged ? <Header /> : <LandingHeader />}
+            {isUserLogged ? <LandingHeader /> :<Auth />}
             {children}
             {/* <Footer /> */}
           </div>
